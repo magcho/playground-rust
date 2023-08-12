6d8c32b7a9874458d88921d472af37d676d4de6a
magcho <mail@magcho.com>
2023-08-13 02:47:13 +0900

    p102

---
+++ webapp_rust_book/my-todo/src/main.rs
@@ -0,0 +1,53 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    tracing::debug!("listebing on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
+
+async fn root() -> &'static str {
+    "Hello, world"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
