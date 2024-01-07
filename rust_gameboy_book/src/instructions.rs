e7d74518fcf94bb3249df28b0698507e6013578c
magcho <mail@magcho.com>
2024-01-07 22:37:38 +0900

    3.3.4 macro

--- rust_gameboy_book/src/instructions.rs
+++ rust_gameboy_book/src/instructions.rs
@@ -3,3 +3,22 @@ impl Cpu {
         self.fetch(bus);
     }
 }
+
+macro_rules! step {
+    ($d: expr, {$($c:tt : $e:expr,)*}) => {
+        static STEP: AtomicU8 = AtomicU8::new(0);
+        #[allow(dead_code)]
+        static VAL8: AtomicU8 = AtomicU8::new(0);
+        #[allow(dead_code)]
+        static VAL16: AtomicU16 = AtomicU16::new(0);
+        $(if STEP.load(Relaxed) == $c { $e })* else {return $d;}
+    };
+}
+pub(crate) use step;
+
+macro_rules! go {
+    ($e: expr) => {
+        STEP.store($e, Relaxed)
+    };
+}
+pub(crate) use go;
