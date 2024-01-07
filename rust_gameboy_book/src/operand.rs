0c4d479b7dc897fcb6af1d12bc0ca9294bb2b4fc
magcho <mail@magcho.com>
2024-01-08 01:29:42 +0900

    3.3.4 direct 16 io

--- rust_gameboy_book/src/operand.rs
+++ rust_gameboy_book/src/operand.rs
@@ -223,3 +223,33 @@ impl IO16<Imm16> for Cpu {
         unreachable!();
     }
 }
+
+impl IO16<Direct16> for Cpu {
+    fn read16(&mut self, _: &Peripherals, _: Direct16) -> Option<u16> {
+        unreachable!();
+    }
+
+    fn write16(&mut self, _: &Peripherals, _: Direct16, val: u16) -> Option<()> {
+        step!(None,{
+            0: if let Some(lo) = self.read8(bus, Imm8){
+                VAL8.store(lo, Relaxed);
+                go!(1);
+            },
+            1: if let Some(hio) = self.read8(bus, Imm8){
+                VAL16.store(u16::from_le_bytes([VAL8.load(Relaxed), hi]),Relaxed);
+                go!(2);
+            },
+            2: {
+                bus.write(VAL16.load(Relaxed), val as u8);
+                go!(3);
+                return None;
+            },
+            3:{
+                bus.write(VAL16.load(Relaxed), wrapping_add(1), (val >> 8) as u8);
+                go!(4);
+                return None;
+            },
+            4: return Some(go!(0)),
+        })
+    }
+}
