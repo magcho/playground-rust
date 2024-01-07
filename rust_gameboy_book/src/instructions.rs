b38b2d42d8bb83a0a09b7410414147e5c24c2902
magcho <mail@magcho.com>
2024-01-08 02:12:35 +0900

    3.3.5 opcode cp

--- rust_gameboy_book/src/instructions.rs
+++ rust_gameboy_book/src/instructions.rs
@@ -40,6 +40,21 @@ impl Cpu {
             }
         })
     }
+
+    // A レジスタと s の値の比較
+    pub fn cp<S: Copy>(&mut self, bus: &Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            let (result, carry) = self.regs.a.overflowing_sub(v);
+
+            self.regs.set_zf(result == 0);
+            self.regs.set_nf(true);
+            self.regs.set_hf((self.regs.a & 0xf) < (v & 0xf));
+            self.regs.set_cf(carry);
+        }
+    }
 }
 
 macro_rules! step {
