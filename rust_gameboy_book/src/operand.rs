46eaa21965eeee40c3b6c1260f0541d467618a2b
magcho <mail@magcho.com>
2024-01-07 22:40:58 +0900

    3.3.4 macro memory read

--- rust_gameboy_book/src/operand.rs
+++ rust_gameboy_book/src/operand.rs
@@ -96,21 +96,18 @@ impl IO8<Reg8> for Cpu {
 }
 impl IO8<Imm8> for Cpu {
     fn read8(&mut self, bus: &Peripherals, _: Imm8) -> Option<u8> {
-        static STEP: AtomicU8 = AtomicU8::new(0);
-        static VAL8: AtomicU8 = AtomicU8::new(0);
-
-        match STEP.load(Relaxed) {
-            0 => {
+        step!(None, {
+            0:{
                 VAL8.store(bus.read(self.regs.pc), Relaxed);
-                self.regs.pc = self.regs.pc.wrapping_add(1);
-                STEP.fetch_add(1, Relaxed);
-                None
+                self.regs.pc = self.regs.wrapping_add(1);
+                go!(1);
+                return None
+            },
+            1:{
+                go!(0);
+                return Some(VAL8.load(Relaxed));
             }
-            1 => {
-                STEP.store(0, Relaxed);
-                Some(VAL8.load(Relaxed))
-            }
-        }
+        })
     }
 
     fn write8(&mut self, _: Imm8, _: u8) {
