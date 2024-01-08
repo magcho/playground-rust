9a8aeb18c77b1153f0aa23a7e7d1c6f09bb37bdc
magcho <mail@magcho.com>
2024-01-08 17:03:14 +0900

    3.3.5 opcode push

--- rust_gameboy_book/src/instructions.rs
+++ rust_gameboy_book/src/instructions.rs
@@ -176,6 +176,49 @@ impl Cpu {
             self.fetch(bus);
         }
     }
+
+    pub fn push16(&mut self, bus: &mut Peripherals, val: u16) -> Option<()> {
+        step!(None,{
+            0: {
+                go!(1);  // cycle数はメモリアクセス数+1
+                return None;
+            },
+            1:{
+                let [lo, hi] = u16::to_le_bytes(val);
+                self.regs.sp = self.regs.sp.wrapping_sub(1); // GBのスタックは上方向(ポインターが減る方向)に積まれる
+                bus.write(self.regs.sp, hi);
+                VAL8.store(lo, Relaxed);
+                go!(2);
+                return None;
+            },
+            2:{
+                self.regs.sp = self.regs.sp.wrapping_sub(1);
+                bus.write(self.regs.sp, VAL8.load(Relaxed));
+                go!(3);
+                return None;
+            },
+            3:{
+                return Some(go!(0));
+            }
+        })
+    }
+
+    pub fn push(&mut self, bus: &mut Peripherals, src: Reg16) {
+        step!((), {
+            0: {
+                VAL16.store(self.read16(bus, src).unwrap(), Relaxed);
+                go!(1);
+            },
+            1: if self.push16(bus, VAL16.load(Relaxed)).is_some(){
+                go!(2);
+            },
+            2:{
+                go!(0);
+                self.fetch(bus);
+            }
+
+        })
+    }
 }
 
 macro_rules! step {
