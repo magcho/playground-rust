aa89cad5111414262336591bf9815c32e8195ce1
magcho <mail@magcho.com>
2024-01-08 17:08:16 +0900

    3.3.5 opcode pop

--- rust_gameboy_book/src/instructions.rs
+++ rust_gameboy_book/src/instructions.rs
@@ -219,6 +219,35 @@ impl Cpu {
 
         })
     }
+
+    pub fn pop16(&mut self, bus: &Peripherals) -> Option<u16> {
+        step!(None, {
+            0: {
+                VAL8.store(bus.read(self.regs.sp), Relaxed);
+                self.regs.sp = self.regs.sp.wrapping_add(1);
+                go!(1);
+                return None;
+            },
+            1: {
+                let hi = bus.read(self.regs.sp);
+                self.regs.sp = self.regs.sp.wrapping_add(1);
+                VAL16.store(u16::from_le_bytes([VAL8.load(Relaxed), hi]), Relaxed);
+                go!(2);
+                return None;
+            },
+            2: {
+                go!(0);
+                return Some(VAL16.load(Relaxed));
+            }
+        })
+    }
+
+    pub fn pop(&mut self, bus: &mut Peripherals, dst: Reg16) {
+        if let Some(v) = self.pop16(bus) {
+            self.write16(bus, dst, v);
+            self.fetch(bus);
+        }
+    }
 }
 
 macro_rules! step {
