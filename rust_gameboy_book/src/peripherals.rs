da9a1e1028d66817e9a6950de6540489c34d6ca5
magcho <mail@magcho.com>
2024-01-11 00:24:24 +0900

    3.4.1 ppu memory mmio mapping

--- rust_gameboy_book/src/peripherals.rs
+++ rust_gameboy_book/src/peripherals.rs
@@ -23,6 +23,11 @@ impl Peripherals {
             }
             0xC000..=0xFDFF => self.wram.read(addr),
             0xFF80..=0xFFFE => self.hram.read(addr),
+
+            0x8000..=0x9FFF => self.ppu.read(addr),
+            0xFE00..=0xFE9F => self.ppu.read(addr),
+            0xFF40..=0xFF4B => self.ppu.read(addr),
+
             _ => 0xFF,
         }
     }
@@ -32,6 +37,11 @@ impl Peripherals {
             0xC000..=0xFDFF => self.wram.write(addr, val),
             0xFF50 => self.bootrom.write(addr, val),
             0xFF80..=0xFFFE => self.hram.write(addr, val),
+
+            0x8000..=0x9FFF => self.ppu.write(addr),
+            0xFE00..=0xFE9F => self.ppu.write(addr),
+            0xFF40..=0xFF4B => self.ppu.write(addr),
+
             _ => (),
         }
     }
