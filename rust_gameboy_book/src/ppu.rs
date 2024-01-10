63b5dd056ab07f26099700c62cf44947b29946c7
magcho <mail@magcho.com>
2024-01-11 00:19:52 +0900

    3.4.1 ppu write

--- rust_gameboy_book/src/ppu.rs
+++ rust_gameboy_book/src/ppu.rs
@@ -38,4 +38,66 @@ impl Ppu {
             mode: Mode::OamScan,
         }
     }
+
+    pub fn read(&self, addr: u16) -> u8 {
+        match addr {
+            0x8000..=0x9xFFF => {
+                if self.mode == Mode::Drawing {
+                    0xFF
+                } else {
+                    self.vram[addr as usize & 0x1FFF]
+                }
+            }
+            0xFE00..=0xFE9F => {
+                if self.mode == Mode::Drawing || self.mode == Mode::OamScan {
+                    0xFF
+                } else {
+                    self.oam[addr as usize & 0xFF]
+                }
+            }
+            0xFF40 => self.lcdc,
+            0xFF41 => 0x80 | self.stat | self.mode as u8,
+            0xFF42 => self.scy,
+            0xFF43 => self.scx,
+            0xFF44 => self.ly,
+            0xFF45 => self.lyc,
+
+            0xFF47 => self.bgp,
+            0xFF48 => self.obp0,
+            0xFF49 => self.obp1,
+            0xFF4A => self.wy,
+            0xFF4B => self.wx,
+
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn write(&mut self, addr: u16, val: u8) {
+        match addr {
+            0x8000..=0x9xFFF => {
+                if self.mode != Mode::Drawing {
+                    self.vram[addr as usize & 0x1FFF] = val;
+                }
+            }
+            0xFE00..=0xFE9F => {
+                if self.mode == Mode::Drawing || self.mode == Mode::OamScan {
+                    self.oam[addr as usize & 0xFF] = val;
+                }
+            }
+            0xFF40 => self.lcdc = val,
+            0xFF41 => self.stat = (self.stat & LYC_EQ_LY) | (val & 0xF8),
+            0xFF42 => self.scy = val,
+            0xFF43 => self.scx = val,
+            0xFF44 => {} // LYレジスタは書き込み不可
+            0xFF45 => self.lyc = val,
+
+            0xFF47 => self.bgp = val,
+            0xFF48 => self.obp0 = val,
+            0xFF49 => self.obp1 = val,
+            0xFF4A => self.wy = val,
+            0xFF4B => self.wx = val,
+
+            _ => unreachable!(),
+        }
+    }
 }
